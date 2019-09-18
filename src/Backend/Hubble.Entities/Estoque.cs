e7ee16b67ec082915321057fa15cf38aaa64e00e
Luiz Guilherme Bandeira <arkanael@hotmail.com>
2019-09-18 19:37:00 -0300

    Inicio de Projeto: Criando as endidades de Estoque e de Produto. Obs.: Mudança do projeto Hubble.Entities da pasta src para src, backend.

---
+++ src/Backend/Hubble.Entities/Estoque.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hubble.Entities
+{
+    public class Estoque
+    {
+        public Estoque(int idEstoque, string nome)
+        {
+            IdEstoque = idEstoque;
+            Nome = nome;
+        }
+
+        public int IdEstoque { get; set; }
+        public string Nome { get; set; }
+        public IList<Produto> Produtos { get; set; }
+    }
+}
